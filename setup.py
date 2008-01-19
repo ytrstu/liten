09393570b8555a0debc4315e9f80f243dac61697
noah.gift <noah.gift@localhost>
2008-01-19 22:40:15 +0000

    adding py_modules section

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ setup(name='liten',
       license='MIT',
       packages=find_packages(),
       zip_safe=False,
-      entry_points="""
       py_modules=['liten']
+      entry_points="""
       [console_scripts]
       liten = liten:main
       """,
